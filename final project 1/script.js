38d206a283ad1d463bfd9c550c6a1b5dadda7ef6
user <levon.avetyan.y@tumo.org>
2019-05-25 11:11:49 +0400

    unbreakable

--- final project 1/script.js
+++ final project 1/script.js
@@ -1,4 +1,6 @@
-
+exanak = "summer";
+let xotiGuyn = "green";
+let horsecol = "blue"
 function setup() {
     var socket = io();
     var side = 50;
@@ -7,8 +9,8 @@ function setup() {
     let grassEaterCount = document.getElementById('grassEaterCount');
     let horseCount = document.getElementById('horseCount');
     let gishatichCount = document.getElementById('gishatichCount');
-
-
+    let summerBtn = document.getElementById("summer");
+    let winterBtn = document.getElementById("winter");
 
     socket.on("data", drawcreatures);
 
@@ -21,40 +23,52 @@ function setup() {
 
         createCanvas(matrix[0].length * side, matrix.length * side)
         background('#acacac');
-    
 
+        summerBtn.onclick = function () {
+            exanak = "summer";
+            xotiGuyn = "green";
+            horsecol = "blue"
+        }
+        
+        winterBtn.onclick = function () {
+            exanak = "winter";
+            xotiGuyn = "#314FAE";
+            horsecol = "#1AAF05"
 
-    
-    for (var i = 0; i < matrix.length; i++) {
-        for (var j = 0; j < matrix[i].length; j++) {
-            if (matrix[i][j] == 1) {
-                fill("green");
-                rect(j * side, i * side, side, side);
-            } else if (matrix[i][j] == 2) {
-                fill("yellow");
-                rect(j * side, i * side, side, side);
-            } else if (matrix[i][j] == 0) {
-                fill('#acacac');
-                rect(j * side, i * side, side, side);
-            }
-            else if (matrix[i][j] == 3) {
-                fill("red");
-                rect(j * side, i * side, side, side);
-            }
-            else if (matrix[i][j] == 4) {
-                fill("blue");
-                rect(j * side, i * side, side, side);
-            }
-            else if (matrix[i][j] == 5) {
-                fill("black");
-                rect(j * side, i * side, side, side);
+        }
+
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 1) {
+                    fill(xotiGuyn);
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 2) {
+                    fill("yellow");
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 0) {
+                    fill('#acacac');
+                    rect(j * side, i * side, side, side);
+                }
+                else if (matrix[i][j] == 3) {
+                    fill("red");
+                    rect(j * side, i * side, side, side);
+                }
+                else if (matrix[i][j] == 4) {
+                    fill(horsecol);
+                    rect(j * side, i * side, side, side);
+                }
+                else if (matrix[i][j] == 5) {
+                    fill("black");
+                    rect(j * side, i * side, side, side);
+                }
             }
         }
+
+
+
+
+
     }
-
-
-  
-}
 }
 
 
